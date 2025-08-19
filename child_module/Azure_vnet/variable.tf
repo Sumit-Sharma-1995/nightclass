54886d0884f0d3f64bb8a43f9edaf24b60a5b7ee
Sumit Dhiman <SumitKumar@rajeevsha83gmail.onmicrosoft.com>
2025-08-19 14:15:21 +0530

    vnet added

---
+++ child_module/Azure_vnet/variable.tf
@@ -0,0 +1,23 @@
+variable "vnet_name" {
+  description = "Name of the resource"
+  type        = string
+  
+}
+
+variable "address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  
+}
+
+variable "location" {
+  description = "Location for the virtual network"
+  type        = string
+  
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group where the virtual network will be created"
+  type        = string
+  
+}
--- parent_module/main.tf
+++ parent_module/main.tf
@@ -16,6 +16,15 @@ module "azurerm_storage_account" {
   account_replication_type = "LRS"
 }
 
+module "azurerm_virtual_network" {
+  depends_on = [module.azurerm_storage_account]
+  source            = "../child_module/Azure_vnet"
+  vnet_name         = "yamlvnet"
+  address_space     = ["10.0.0.0/16"]
+  location          = "East US"
+  resource_group_name = module.azurerm_resource_group.azurerm_resource_group.name
+  
+}
 import {
   to = module.azurerm_resource_group.azurerm_resource_group.discussion
   id = "/subscriptions/2775dd26-3d9b-4b17-b5d0-b8edea927622/resourceGroups/yamlrg"
